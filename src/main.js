f0fe14feb56df9e8c31eea91679b3702c301a818
golden <120225883@qq.com>
2018-07-06 15:05:02 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,35 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import './theme/default.styl';
+import 'font-awesome/css/font-awesome.css';
+Vue.config.productionTip = false;
+Vue.use(Vuetify, {
+  // theme: {
+  //   primary: colors.indigo.base, // #E53935
+  //   secondary: colors.indigo.lighten4, // #FFCDD2
+  //   accent: colors.indigo.base // #3F51B5
+  // },
+  options: {
+    themeVariations: ['primary', 'secondary', 'accent'],
+    extra: {
+      mainToolbar: {
+        color: 'primary',
+      },
+      sideToolbar: {},
+      sideNav: 'primary',
+      mainNav: 'primary lighten-1',
+      bodyBg: '',
+    }
+  }
+});
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: {App},
+  template: '<App/>'
+})
---
+++ src/router/paths.js
@@ -0,0 +1,18 @@
+export default [{
+  path: '/',
+  meta: {},
+  name: 'Root',
+  redirect: {
+    name: 'Dashboard'
+  }
+},
+  {
+    path: '/dashboard',
+    meta: {breadcrumb: true},
+    name: 'Dashboard',
+    component: () => import(
+      /* webpackChunkName: "routes" */
+      /* webpackMode: "lazy" */
+      `@/pages/Dashboard.vue`
+      )
+  },]
