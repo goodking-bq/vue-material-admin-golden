0fa86cbaa4ef7c031f34f3c067ea607e19c91ecf
golden <120225883@qq.com>
2018-07-09 18:09:55 +0800

    jspider

---
+++ src/pages/Project.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :search="search"
+      :pagination.sync="pagination"
+      hide-actions
+      class="elevation-1"
+    >
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Project",
+    data() {
+      return {
+        headers: [{text:"项目名"}]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
